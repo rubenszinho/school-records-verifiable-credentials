f0e3939a05b0edb744ef0ffe9d7adec2044b77ed
root <root@Mumuzinho>
2024-11-11 13:45:07 -0300

    feat: implement blockchain certificate

---
+++ cert-blockchain/contracts/CertificateRegistry.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract CertificateRegistry {
+    address public owner;
+
+    mapping(address => bool) public authorizedIssuers;
+    mapping(bytes32 => bool) public certificates;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    modifier onlyAuthorizedIssuer() {
+        require(authorizedIssuers[msg.sender], "Not an authorized issuer");
+        _;
+    }
+
+    function addAuthorizedIssuer(address issuer) public onlyOwner {
+        authorizedIssuers[issuer] = true;
+    }
+
+    function issueCertificate(
+        bytes32 certificateHash
+    ) public onlyAuthorizedIssuer {
+        certificates[certificateHash] = true;
+    }
+
+    function verifyCertificate(
+        bytes32 certificateHash
+    ) public view returns (bool) {
+        return certificates[certificateHash];
+    }
+}
